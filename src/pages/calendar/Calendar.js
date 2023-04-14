21fb31de1ad90d2c9d6cce7334092690af4e0e50
Viverino1 <Vivek.maddineni@gmail.com>
2023-04-14 16:08:38 -0500

    added default date

--- src/pages/calendar/Calendar.js
+++ src/pages/calendar/Calendar.js
@@ -1,8 +1,18 @@
 import DayView from "./components/dayView/DayView";
 import Menu from "./components/menu/Menu";
+import { useSelector, useDispatch } from 'react-redux'
+import { setDisplayDay, setDisplayMonth, incrementDisplayMonth, decrementDisplayMonth } from './components/months/monthsSlice'
+
 const Calendar = () => {
+    const dispatch = useDispatch()
+
+    const setCurrentDate = () => {
+        const date = new Date();
+        dispatch(setDisplayDay(date.getDate()))
+        dispatch(setDisplayMonth(date.getMonth() + 1))
+    }
     return(
-        <div className="flex h-[calc(100vh-theme(space.16))]">
+        <div className="flex h-[calc(100vh-theme(space.16))]" onLoad={setCurrentDate()}>
             <div className="relative top-0 left-0 bottom-0">
                 <Menu/>
             </div>
